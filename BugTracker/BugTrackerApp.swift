ec095a82ff9c3af2d1700f86d4c20558a65542d6
jouji <0zt4x92r388082h@ezweb.ne.jp>
2025-12-04 22:56:27 +0900

    Initial Commit

---
+++ BugTracker/BugTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugTrackerApp.swift
+//  BugTracker
+//
+//  Created by 高村直也 on 2025/12/04.
+//
+
+import SwiftUI
+
+@main
+struct BugTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BugTracker
+//
+//  Created by 高村直也 on 2025/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
